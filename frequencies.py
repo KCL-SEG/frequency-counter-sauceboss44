aeec327dfb93ad924032579751b172001847facc
sauceboss44 <alexrudakov5@gmail.com>
2023-09-30 16:20:09 +0100

    Fix solution

--- frequencies.py
+++ frequencies.py
@@ -2,5 +2,5 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {i:items.count(i) for i in items}
+    frequencies = {str(i):items.count(str(i)) for i in items}
     return frequencies
